f978df01d4238f49e4d77912588ec2ef7d3503d9
John Watson <outlawstar4761@gmail.com>
2019-11-21 16:16:52 -0600

    abstracting category patterns

---
+++ Models/CategoryPattern.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once __DIR__ . '/../Libs/Record/Record.php';
+
+class CategoryPattern{
+
+  const DB = 'Wu_2';
+  const TABLE = 'category_patterns';
+  const PRIMARYKEY = 'id';
+
+  public $id;
+  public $category_id;
+  public $label;
+  public $pattern;
+
+  public function __construct($id = null){
+    parent::__construct(self::DB,self::TABLE,self::PRIMARYKEY,$id);
+  }
+}
