5017020a17476d9237c32930e8919a6e448fe034
John Watson <outlawstar4761@gmail.com>
2020-02-26 11:11:14 -0600

    adding new class

---
+++ Processes/PayPalReceipt.php
@@ -0,0 +1,73 @@
+<?php
+
+require_once __DIR__ . '/../Libs/Imap.php';
+require_once __DIR__ . '/../Models/Transaction.php';
+
+class PayPalReceipt extends Imap{
+
+  const AMNTPATT = '/([0-9]{1,5}\.[0-9]{2})\sUSD/';
+  const MSGRECPATT = '/Received:.*;\s(.*)/';
+  const FROMADD = 'service@paypal.com';
+  const SUBJPATT = '/Receipt\sfor\sYour\sPayment\sto/';
+
+  public function __construct($host,$user,$pass,$port){
+    parent::__construct($host,$user,$pass,$port);
+    $this->_parse();
+  }
+  protected function _parse(){
+    $results = $this->search('FROM',self::FROMADD);
+    foreach($results as $result){
+      if(preg_match(self::SUBJPATT,$result[0]->subject)){
+        $head = $this->getMsg($result[0]->msgno,0);
+        $body = $this->getMsg($result[0]->msgno,1);
+        $html = base64_decode($body);
+        try{
+          $receivedDate = $this->_parseMsgReceived($head);
+          $dollars = $this->_parseDollarAmnt($html);
+          print_r($this->_getTransaction($dollars,$receivedDate));
+          //echo $receivedDate . "\n" . $dollars . "\n";
+        }catch(\Exception $e){
+          echo $e->getMessage() . "\n";
+        }
+        exit;
+      }
+    }
+    return $this;
+  }
+  protected function _parseDollarAmnt($htmlStr){
+    if(!preg_match_all(self::AMNTPATT,$htmlStr,$matches)){
+      throw new \Exception('Unable to match dollar amount.');
+    }
+    return $matches[1][0];
+  }
+  protected function _parseMsgReceived($str){
+    if(!preg_match(self::MSGRECPATT,$str,$matches)){
+      throw new \Exception('Unable to match received date.');
+    }
+    return date('Y-m-d H:i:s',strtotime($matches[1]));
+  }
+  protected function _getTransaction($amnt,$date){
+    $obj = null;
+    $results = $GLOBALS['db']
+      ->database(Transaction::DB)
+      ->table(Transaction::TABLE)
+      ->select(Transaction::PRIMARYKEY)
+      ->where("memo","like","'paypal'")
+      ->andWhere("amount","=",$amnt)
+      ->andWhere("date","=","'" . $date . "'")
+      ->get();
+    if(!mysqli_num_rows($results)){
+      throw new \Exception('Unable to find source transaction');
+    }
+    while($row = mysqli_fetch_assoc($results)){
+      $obj = new Transasction($row[Transaction::PRIMARYKEY]);
+    }
+    return $obj;
+  }
+}
+
+$host = 'imap.gmail.com';
+$user = 'outlawstar4761@gmail.com';
+$pass = 'B00TSw34t';
+$port = 993;
+$proc = new PayPalReceipt($host,$user,$pass,$port);
